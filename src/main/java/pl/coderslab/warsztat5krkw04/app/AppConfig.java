63d4d973d45813893df768f3df1d978f08c1358d
soul <ykrainec1991@gmail.com>
2020-06-21 15:00:00 +0200

    add some files

--- src/main/java/pl/coderslab/warsztat5krkw04/app/AppConfig.java
+++ src/main/java/pl/coderslab/warsztat5krkw04/app/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,31 +17,19 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 
+
 @Configuration
-@ComponentScan(basePackages = "pl.coderslab.warsztat5krkw04")
+@ComponentScan("pl.coderslab.warsztat5krkw04")
 @EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
-        stringConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "plain",
-                Charset.forName("UTF-8"))));
-        converters.add(stringConverter);
-    }
-
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver =
-                new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
-
-    @Override
-    public void configureDefaultServletHandling(
-            DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
+    @Configuration
+    @EnableWebMvc
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE");
+//                    .allowedOrigins("http://localhost");
+        }
     }
 }
-
